2241cda947baa8584847505aca375ab3db4a8de4
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2026-06-12 14:40:31 +0300

    Initial Commit

---
+++ WeatherCast App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherCast App
+//
+//  Created by albaraa alsayed on 26/12/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherCast App/WeatherCast_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCast_AppApp.swift
+//  WeatherCast App
+//
+//  Created by albaraa alsayed on 26/12/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCast_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
