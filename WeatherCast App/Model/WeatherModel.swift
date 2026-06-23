abcd4b0054645e14a8e287e8515dd8069cc4ec51
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2026-06-23 12:51:19 +0300

    first commit

---
+++ WeatherCast App/Model/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  WeatherCast App
+//
+//  Created by albaraa alsayed on 08/01/1448 AH.
+//
+
+import Foundation
---
+++ WeatherCast App/Utils/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  WeatherCast App
+//
+//  Created by albaraa alsayed on 26/12/1447 AH.
+//
+
+import Foundation
