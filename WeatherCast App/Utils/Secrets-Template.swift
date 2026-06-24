a88c07457028e358dff7faa1a4c8d7747cc93d73
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2026-06-25 00:01:01 +0300

    fix seacrit problem

--- WeatherCast App/Utils/Secrets-Template.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct Secrets {
-    // ⚠️ IMPORTANT: Replace this with your actual WeatherAPI key.
-    // Get one at https://www.weatherapi.com
-    static let apiKey = "YOUR_API_KEY_HERE"
-}
